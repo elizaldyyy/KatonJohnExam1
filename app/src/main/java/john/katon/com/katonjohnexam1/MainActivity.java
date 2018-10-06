8d144781013e451c15a2da58c376a1a96dcd09cb
John Elizaldy Katon <2015086831@ust-ics.mygbiz.com>
2018-10-06 19:38:43 +0800

    Initial commit

---
+++ app/src/main/java/john/katon/com/katonjohnexam1/MainActivity.java
@@ -0,0 +1,13 @@
+package john.katon.com.katonjohnexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
